d3a9013b10bb35b20328f2e2e47b4197c830b935
metee_sri <lieangloveu@gmail.com>
2018-06-05 16:38:02 +0700

    add test case

--- com.learn.ci.junit/src/test/java/com/learn/ci/junit/client/SimpleTest.java
+++ com.learn.ci.junit/src/test/java/com/learn/ci/junit/client/SimpleTest.java
@@ -2,7 +2,7 @@ package com.learn.ci.junit.client;
 
 import org.junit.Test;
 
-public class SimpleTest {
+public class SimpleTest { 
 
 	@Test
 	public void  testCase1() {
@@ -10,5 +10,71 @@ public class SimpleTest {
 		int j = 2;
 		assert(i+j == 3);
 	}
+	
+	
+	@Test
+	public void  testCase2() {
+		int i = 1 ;
+		int j = 2;
+		assert(i+j == 3);
+	}
+	
+	
+	@Test
+	public void  testCase3() {
+		int i = 1 ;
+		int j = 2;
+		assert(i+j == 3);
+	}
+	
+	
+	
+	
+	@Test
+	public void  testCase4() {
+		int i = 1 ;
+		int j = 2;
+		assert(i+j == 3);
+	}
+	
+	
+	
+	
+	@Test
+	public void  testCase5() {
+		int i = 1 ;
+		int j = 2;
+		assert(i+j == 3);
+	}
+	
+	
+	
+	
+	
+	@Test
+	public void  testCase6() {
+		int i = 1 ;
+		int j = 2;
+		assert(i+j == 3);
+	}
+	
+	
+	
+	@Test
+	public void  testCase7() {
+		int i = 1 ;
+		int j = 2;
+		assert(i+j == 1);
+	}
+	
+	
+	
+	@Test
+	public void  testCase8() {
+		int i = 1 ;
+		int j = 2;
+		assert(i+j == 1);
+	}
+	
 	
 }
